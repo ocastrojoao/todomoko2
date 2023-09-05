022f453a85e1afa97302273d50de614e867ffd05
ocastrojoao <comoquefala@gmail.com>
2023-09-05 08:40:54 -0300

    tasks and save informations

--- todomoko2/src/app/app.component.ts
+++ todomoko2/src/app/app.component.ts
@@ -1,21 +1,29 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    taskText: string | undefined;
-    tasks: string[] = [];
+  taskText: string = '';
+  tasks: { text: string, completed: boolean }[] = [];
 
-    saveTask(): void {
-        if (!this.taskText) {
-            console.log('no task to save');
-            return;
-        }
-        this.tasks.push(this.taskText);
-        this.taskText = undefined;
+  get completedTasks() {
+    return this.tasks.filter(task => task.completed);
+  }
+
+  get uncompletedTasks() {
+    return this.tasks.filter(task => !task.completed);
+  }
+
+  saveTask(): void {
+    if (!this.taskText) {
+      console.log('no task to save');
+      return;
     }
+    this.tasks.push({text: this.taskText, completed: false});
+    this.taskText = '';
+  }
 }
 
