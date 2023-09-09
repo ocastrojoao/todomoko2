e96a3f55fdbee1d0738d8d3d6ca00cfebf981c05
ocastrojoao <comoquefala@gmail.com>
2023-09-09 20:42:06 -0300

    local storage update

--- todomoko2/src/app/app.component.ts
+++ todomoko2/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task} from "./interfaces/task.interface";
 
 @Component({
@@ -6,10 +6,17 @@ import {Task} from "./interfaces/task.interface";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   taskText: string = '';
   tasks: Task[] = [];
   showError: boolean = false;
+
+  ngOnInit() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
 
   get completedTasks() {
     return this.tasks.filter(task => task.completed);
@@ -27,6 +34,17 @@ export class AppComponent {
     this.tasks.push({text: this.taskText, completed: false});
     this.taskText = '';
     this.showError = false;
+
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  deleteTask(task: Task): void {
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
   }
 }
+
 
